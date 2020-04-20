f6c1bf4ecb2c98da1eada9cf23bdbafc8a595907
RK <ponradha@gmail.com>
2020-04-20 22:50:12 +0530

    Pipes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  todayDate = new Date();
   headerClass = 'user-list-header';
   usersList: any = [
     {
